676b8fd0f996259490a4af1cf0bf25cb80299daf
qa-auto-n <nastassia.ilyich@gmail.com>
2025-02-04 14:47:26 +0400

    refactor: move save dialog to components

--- src/app/pages/components-page.ts
+++ src/app/pages/components-page.ts
@@ -2,11 +2,13 @@ import {SideBarElement} from '../components/layout-elements/side-bar-element';
 import {TabActionBarElement} from '../components/layout-elements/tab-action-bar-element';
 import {getOrCreate} from '../../core/utils/init-utils';
 import {HeaderElement} from '../components/layout-elements/header-bar-element';
+import {DialogElement} from '../components/layout-elements/dialog-element';
 
 export abstract class ComponentsPage {
     private static _header: HeaderElement | null = null;
     private static _sideBar: SideBarElement | null = null;
     private static _tabActionBar: TabActionBarElement | null = null;
+    private static _saveDialog: DialogElement | null = null;
 
     public static get header(): HeaderElement {
         return getOrCreate(this._header, HeaderElement.getHeader);
@@ -18,5 +20,9 @@ export abstract class ComponentsPage {
 
     public static get tabActionBar(): TabActionBarElement {
         return getOrCreate(this._tabActionBar, TabActionBarElement.getTabActionBar);
+    }
+
+    public static get saveDialog(): DialogElement {
+        return getOrCreate(this._saveDialog, DialogElement.getSaveDialog);
     }
 }
